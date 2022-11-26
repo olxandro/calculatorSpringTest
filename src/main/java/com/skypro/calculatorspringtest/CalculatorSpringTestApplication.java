abb7b00be3bb6df57ba695ad7539c612dbcf84ee
Admin O <olexandro@gmail.com>
2022-11-26 11:44:14 +0300

    Initial commit

---
+++ src/main/java/com/skypro/calculatorspringtest/CalculatorSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.calculatorspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorSpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/calculatorspringtest/CalculatorSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.calculatorspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
